6abda98459dd49769855996b4c59ad0b7cbe12e4
Intisar Hussain Jaafar <Intesarabdullah67@gmail.com>
2026-03-29 11:27:05 +0300

    customer/agent models ready

---
+++ server.js
@@ -0,0 +1,15 @@
+const dns = require("dns")
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+
+require("dotenv").config({ quiet: true })
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const path = require("path")
+const app = express()
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`My server is listening on port ${PORT} =>`)
+})
