42dc3fab6b519e2ca80251dd8f5822ce477fc72d
abeer-rozba <abeerrozba3@gmail.com>
2026-03-29 14:28:11 +0300

    modify Agent model - removed property field

--- models/Agent.js
+++ models/Agent.js
@@ -1,19 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 const agentSchema = new mongoose.Schema(
   {
     username: { type: String, required: true },
     password: { type: String, required: true },
-    property: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Property",
-      required: true,
-    },
     phoneNum: { type: Number, required: true },
     email: { type: String, required: true },
-    company: { type: String, required: true },
+    company: { type: String, required: true }
   },
   { timestamps: true }
 )
 
-module.exports = mongoose.model("Agent", agentSchema)
+module.exports = mongoose.model('Agent', agentSchema)
