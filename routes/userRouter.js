ad7adbc8f2342255068b60d40bf4bae36ecd631a
Intisar Hussain Jaafar <Intesarabdullah67@gmail.com>
2026-03-31 15:04:57 +0300

    profile functions are working and

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,7 +4,6 @@ const userController = require('../controllers/userController.js')
 
 router.get("/:id", userController.showProfile)
 router.put("/:id/edit", userController.editProfile)
-// router.put("/:id/edit", userController.updateProfile)
 // router.get("/bookmarks", userController.getAllBookmarks)
 
 module.exports = router
